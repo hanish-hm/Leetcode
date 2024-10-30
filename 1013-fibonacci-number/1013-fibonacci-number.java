387c741f4d306b0feeb6db908fc6eb9b82ef83f4
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2024-10-30 17:31:22 +0530

    Time: 0 ms (100%), Space: 40.6 MB (18.9%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,8 +1,16 @@
 class Solution {
     public int fib(int n) {
-        if(n==1 || n==0){
+        if(n==0 || n==1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        int prev = 1;
+        int prev2 = 0;
+        int curr;
+        for(int i=2;i<=n;i++){
+            curr = prev2+prev;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 }
