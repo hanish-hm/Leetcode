cf7d688977e325664dee25389add91a7657f4359
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2024-12-21 23:49:46 +0530

    Time: 0 ms (100%), Space: 13.4 MB (31.13%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> m;
-        
-        for(auto x: nums){
-            m[x]++;
+        sort(nums.begin(),nums.end());
+        if(nums.size() == 1){
+            return nums[0];
         }
-
-        for(auto x: m){
-            if(x.second == 1){
-                return x.first;
+        for(int i=1;i<nums.size();i+=3){
+            if(nums[i-1]!=nums[i]){
+                return nums[i-1];
+            }
+            if(nums[i+1]!=nums[i]){
+                return nums[i+1];
             }
         }
-        
-        return -1;
+        return nums[nums.size()-1];
     }
 };
