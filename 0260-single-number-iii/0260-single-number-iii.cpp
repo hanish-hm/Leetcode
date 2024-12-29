87601e94f74dce188a7e336ba2ce85de4adc23c2
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2024-12-30 00:16:52 +0530

    Time: 0 ms (100%), Space: 13.9 MB (58.95%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -6,7 +6,7 @@ public:
             xornum = xornum^it;
         }
         unsigned int xornum_unsigned = static_cast<unsigned int>(xornum);
-        unsigned int rightsetbit = xornum_unsigned & -xornum_unsigned;
+        unsigned int rightsetbit = xornum_unsigned^(xornum_unsigned&(xornum_unsigned-1));
         int a = 0;
         int b = 0;
         for(auto it:nums){
