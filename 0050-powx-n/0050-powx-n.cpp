a78e8834382375082ba890f8e3b6588d9ef45842
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2025-01-05 13:12:12 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.27%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x, n);
+    double myPow(double x, int b) {
+        int a = b;
+        long long n = abs((long long)b);
+        double ans = 1;
+        while(n>0){
+            if(n%2!=0){
+                ans *= x;
+                n -= 1;
+            }
+            else{
+                x *= x;
+                n /= 2;
+            }
+        }
+        if(a < 0){
+            return 1.0/ans;
+        }
+        return ans;
     }
 };
