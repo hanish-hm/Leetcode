2507c8d4d1c36f9cafe384df6bb4480079ba406e
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2025-02-11 11:52:37 +0530

    Time: 30 ms (0.84%), Space: 31.3 MB (45.15%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if(nums.size() == 1 && nums[0] != target){
-            return -1;
-        }
         int low = 0;
         int high = nums.size()-1;
         while(low<=high){
