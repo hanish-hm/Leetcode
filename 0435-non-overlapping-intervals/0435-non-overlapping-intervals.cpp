5d95bc384d61bebcbd377cb3632c972a86fb98fa
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2025-06-11 02:57:54 +0530

    Time: 31 ms (98.57%), Space: 100.6 MB (23.84%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -3,9 +3,17 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         int n = intervals.size();
         int count = 0;
-        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
-        return a[1] < b[1];
-    });
+        vector<pair<int,int>> pr;
+        for(int i=0;i<n;i++){
+            pr.push_back({intervals[i][1],intervals[i][0]});
+        }
+        sort(pr.begin(),pr.end());
+        int x = 0;
+        for(auto it:pr){
+            intervals[x][0] = it.second;
+            intervals[x][1] = it.first;
+            x++;
+        }
         int val = intervals[0][1];
         for(int i=1;i<n;i++){
             if(intervals[i][0]<val){
