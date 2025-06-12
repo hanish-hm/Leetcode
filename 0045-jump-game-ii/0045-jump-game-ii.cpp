cdeb193615e5670974f096b066a6a70b8d85ab15
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2025-06-13 03:01:56 +0530

    Time: 0 ms (100%), Space: 20.4 MB (84.86%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1){
+            return 0;
+        }
+        int jumps = 0;
+        int farthest = 0;
+        int currentend = 0;
+        for(int i=0;i<n;i++){
+            farthest = max(farthest,i+nums[i]);
+            if(i==currentend){
+                jumps++;
+                currentend = farthest;
+                if(currentend >= n-1){
+                    break;
+                }
+            }
+        }
+        return jumps;
+    }
+};
