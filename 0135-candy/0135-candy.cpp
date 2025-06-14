ccc7e8f1fc5dbdd7a4ac4df2bb047da994b9b5bb
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2025-06-15 04:46:58 +0530

    Time: 0 ms (100%), Space: 22.9 MB (46.38%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,9 +3,9 @@ public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
         vector<int> a(n);
-        vector<int> b(n);
         a[0] = 1;
-        b[n-1] = 1;
+        int prev = 1;
+        int curr = 0;
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
                 a[i] = a[i-1]+1;
@@ -14,18 +14,19 @@ public:
                 a[i] = 1;
             }
         }
+        int sum = max(a[n-1],1);
         for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]){
-                b[i] = b[i+1]+1;
+                curr = prev+1;
+                prev= curr;
             }
             else{
-                b[i] = 1;
+                curr = 1;
+                prev = curr;
             }
+            sum += max(a[i],curr);
         }
-        int sum = 0;
-        for(int i=0;i<n;i++){
-            sum += max(a[i],b[i]);
-        }
+        
         return sum;
     }
 };
