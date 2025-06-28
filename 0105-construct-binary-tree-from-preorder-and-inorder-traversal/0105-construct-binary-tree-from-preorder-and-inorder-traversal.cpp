b8c5705248f76ad6dd15e9ea39b79bb67684c7ef
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2025-06-29 04:19:09 +0530

    Time: 1 ms (84.5%), Space: 27.6 MB (20.55%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* func(vector<int>& preorder,int prestart, int preend, vector<int>& inorder, int instart, int inend, unordered_map<int,int>& mp){
+        if(prestart>preend || instart>inend){
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(preorder[prestart]);
+        int betin = mp[root->val];
+        int left  = betin-instart;
+        root->left = func(preorder,prestart+1,prestart+left,inorder,instart,betin-1,mp);
+        root->right = func(preorder,prestart+left+1,preend,inorder,betin+1,inend,mp);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n= inorder.size()-1;
+        unordered_map<int,int> mp;
+        for(int i=0;i<inorder.size();i++){
+            mp[inorder[i]] = i;
+        }
+        TreeNode* root = func(preorder,0,n,inorder,0,n,mp);
+        return root;
+    }
+};
