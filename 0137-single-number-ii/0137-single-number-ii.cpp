babca3e4d39c25fb3e2aa36b09e9671d851cf24e
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2025-07-03 22:34:17 +0530

    Time: 1 ms (45.05%), Space: 13.2 MB (98.64%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -3,15 +3,18 @@ public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
         int ans = 0;
-        for(int i=0;i<32;i++){
-            int count  = 0;
-            for(int j=0;j<n;j++){
-                if(nums[j]&(1<<i)){
+        for(int bit=0;bit<32;bit++){
+            int count = 0;
+            for(int i=0;i<n;i++){
+                if(nums[i]&(1<<bit)){
                     count++;
                 }
             }
-            if(count%3!=0){
-                ans = ans|(1<<i);
+            if(count%3==0){
+                ans |= 0<<bit;
+            }
+            else{
+                ans|=1<<bit;
             }
         }
         return ans;
