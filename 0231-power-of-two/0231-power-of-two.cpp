ebd4f7b8f0a1019f5b95c2fd339d37a0af7b194b
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2025-07-21 12:34:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.6%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0){
+
+        if(n==0){
             return false;
         }
-        return (n&(n-1)) == 0;
+        if(n==1){
+            return true;
+        }
+        return (n%2==0)?isPowerOfTwo(n/2):false;
     }
 };
