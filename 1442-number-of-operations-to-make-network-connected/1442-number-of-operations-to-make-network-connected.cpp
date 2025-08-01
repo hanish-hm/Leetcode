946a956faf92d7eaf5375c852dcbebc0fb5b87f3
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2025-08-01 15:25:21 +0530

    Time: 72 ms (5.09%), Space: 76.4 MB (5.02%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,81 +1,39 @@
-class Disjointset{
-    public: 
-    vector<int> parent,rank,size;
-    Disjointset(int n){
-        parent.resize(n+1);
-        rank.resize(n+1,0);
-        size.resize(n+1);
-        for(int i=0;i<=n;i++){
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-    int findulparent(int x){
-        if(parent[x] == x){
-            return x;
-        }
-        return parent[x] = findulparent(parent[x]);
-    }
-    void unionbyrank(int u, int v){
-        int ulp_u = findulparent(u);
-        int ulp_v = findulparent(v);
-        if(ulp_u == ulp_v) {
-            return;
-        }
-        if(rank[ulp_u] < rank[ulp_v]){
-            parent[ulp_u] = ulp_v;
-        }
-        else if(rank[ulp_v] < rank[ulp_u]){
-            parent[ulp_v] = ulp_u;
-        }
-        else{
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-    void unionbysize(int u,int v){
-        int ulp_u = findulparent(u);
-        int ulp_v = findulparent(v);
-        if(ulp_u == ulp_v) {
-            return;
-        }
-        if(size[ulp_u] < size[ulp_v]){
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else{
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-    }
-};
-
 class Solution {
 public:
+    void bfs(int computer,vector<bool>& vis,vector<vector<int>>& v){
+        queue<int> q;
+        q.push(computer);
+        vis[computer] = true;
+        while(!q.empty()){
+            int computernumber = q.front();
+            q.pop();
+            for(auto it:v[computernumber]){
+                if(!vis[it]){
+                    q.push(it);
+                    vis[it] = true;
+                }
+            }
+        }
+        return;
+    }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        Disjointset ds(n);
+        int cables = connections.size();
         int count = 0;
-        for(auto it:connections){
-            int u = it[0];
-            int v = it[1];
-            if(ds.findulparent(u) == ds.findulparent(v)){
-                count++;
-            }
-            else{
-                ds.unionbysize(u,v);
-            }
-        }
-        int components = 0;
-        for(int i=0;i<n;i++){
-            if(ds.parent[i] == i){
-                components++;
-            }
-        }
-        if(count>=components-1){
-            return components-1;
-        }
-        else{
+        if(cables<n-1){
             return -1;
         }
+        vector<vector<int>> v(n);
+        for(auto c:connections){
+            v[c[0]].push_back(c[1]);
+            v[c[1]].push_back(c[0]);
+        }
+        vector<bool> vis(n,false);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                bfs(i,vis,v);
+                count++;
+            }
+        }
+        return count-1;
     }
 };
