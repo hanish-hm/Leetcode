60b7263a4d25933a88ede64b37b2cccd197fa0c3
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2025-08-15 18:38:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (52.41%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==0){
-            return false;
-        }
         if(n==1){
             return true;
         }
-        return (n%4==0)?isPowerOfFour(n/4):false;
+        if(n==0 || n%4!=0){
+            return false;
+        }
+
+        return isPowerOfFour(n/4);
     }
 };
