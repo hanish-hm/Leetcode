783852a846078e4fda3bc5299c4adfddc6add240
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2025-08-18 23:51:19 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.03%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void fn(vector<vector<int>>& ans,unordered_map<int,int>& mp,int k,int n,vector<int>& x,int sum,int index){
+    void fn(vector<vector<int>>& ans,int k,int n,vector<int>& x,int sum,int index){
         if(sum > n || x.size()>k){
             return;
         }
@@ -9,23 +9,15 @@ public:
             return;
         }
         for(int i=index;i<10;i++){
-            if(!mp[i]){
-                x.push_back(i);
-                mp[i] = 1;
-                fn(ans,mp,k,n,x,sum+i,i+1);
-                mp[i] = 0;
-                x.pop_back();
-            }
+            x.push_back(i);
+            fn(ans,k,n,x,sum+i,i+1);
+            x.pop_back();           
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
-        unordered_map<int,int> mp;
-        for(int i=1;i<10;i++){
-            mp[i] = 0;
-        }
         vector<int> x;
-        fn(ans,mp,k,n,x,0,1);
+        fn(ans,k,n,x,0,1);
         return ans;
     }
 };
