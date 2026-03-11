da13e21a3ee8d0853961752384d9bd7334ab5537
Hanish Mittal <123793511+hanish-hm@users.noreply.github.com>
2026-03-12 00:11:36 +0530

    Time: 0 ms (100%), Space: 7.9 MB (26.22%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -4,16 +4,16 @@ public:
         if(n == 0){
             return 1;
         }
-        int nn = n;
         int i =0;
         int ans = 0;
         while(n!=1){
-            ans+= 1<<i;
+            if(n%2==0){
+                ans+= 1<<i;
+            }
             n = n/2;
             i++;
         }
-        ans+= 1<<i;
 
-        return ans^nn;
+        return ans;
     }
 };
